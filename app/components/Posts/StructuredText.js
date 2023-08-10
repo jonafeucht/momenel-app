c62ce2a19854abe2c3eb41ce4ff6712d349835d0
farhan haider <fhaider085@gmail.com>
2023-08-10 10:36:53 -0400

    bug(caption): link not capturing fixed

--- app/components/Posts/StructuredText.js
+++ app/components/Posts/StructuredText.js
@@ -2,90 +2,88 @@ import React, { memo } from "react";
 import CustomText from "../customText/CustomText";
 import { useBoundStore } from "../../Store/useBoundStore";
 
-const StructuredText = memo((props) => {
-  const mode = useBoundStore((state) => state.mode);
-  const prepareText = (text, mentionHashtagPress) => {
-    const result = [];
-    let shouldAddMore = false;
+const StructuredText = memo(
+  ({ maxCharCount, style, children, mentionHashtagPress }) => {
+    const mode = useBoundStore((state) => state.mode);
 
-    // if number of characters is greater than 400, then replace the last 4 characters with "..."
-    let tempStr;
-    if (text.length > props.maxCharCount) {
-      tempStr = text.substring(0, props.maxCharCount) + "...";
-      shouldAddMore = true;
-    } else {
-      tempStr = text;
-      shouldAddMore = false;
-    }
+    const prepareText = (text, mentionHashtagPress) => {
+      const result = [];
+      let shouldAddMore = false;
 
-    let mentList = tempStr.match(/(https:\/\/www|#|@|www)(\S+)/gi);
+      let tempStr;
+      if (text.length > maxCharCount) {
+        tempStr = text.substring(0, maxCharCount) + "...";
+        shouldAddMore = true;
+      } else {
+        tempStr = text;
+        shouldAddMore = false;
+      }
 
-    if (mentList == null) {
-      if (shouldAddMore) {
+      let mentList = tempStr.match(/(https?:\/\/\S+|www\.\S+|@\S+|#\S+)/gi);
+
+      if (mentList == null) {
+        if (shouldAddMore) {
+          result.push(tempStr);
+          result.push(
+            <Mention
+              key={"more"}
+              mentionHashtagColor={"gray"}
+              mentionHashtagPress={mentionHashtagPress}
+              text={"more"}
+              style={style}
+            />
+          );
+          return result;
+        } else {
+          return [tempStr];
+        }
+      }
+
+      let i = 0;
+      for (const ment of mentList) {
+        i++;
+        result.push(tempStr.substring(0, tempStr.indexOf(ment)));
+        result.push(
+          <Mention
+            key={i}
+            mentionHashtagColor={mode === "dark" ? "#986BFF" : "#8759F2"}
+            mentionHashtagPress={mentionHashtagPress}
+            text={ment}
+            style={style}
+          />
+        );
+        tempStr = tempStr.substring(tempStr.indexOf(ment) + ment.length);
+      }
+      if (tempStr.length > 0) {
         result.push(tempStr);
+      }
+
+      if (shouldAddMore) {
         result.push(
           <Mention
             key={"more"}
-            mentionHashtagColor={"gray"}
             mentionHashtagPress={mentionHashtagPress}
             text={"more"}
-            style={props.style}
           />
         );
-
-        return result;
-      } else {
-        return [tempStr];
       }
-    }
-    let i = 0;
-    for (const ment of mentList) {
-      i++;
+      return result;
+    };
 
-      result.push(tempStr.substring(0, tempStr.indexOf(ment)));
-      result.push(
-        <Mention
-          key={i}
-          mentionHashtagColor={mode === "dark" ? "#986BFF" : "#8759F2"}
-          mentionHashtagPress={mentionHashtagPress}
-          text={ment}
-          style={props.style}
-        />
-      );
-      tempStr = tempStr.substring(
-        tempStr.indexOf(ment) + ment.length,
-        tempStr.length
-      );
-    }
-    if (tempStr.length > 0) {
-      result.push(tempStr);
-    }
-
-    if (shouldAddMore) {
-      result.push(
-        <Mention
-          key={"more"}
-          mentionHashtagPress={mentionHashtagPress}
-          text={"more"}
-        />
-      );
-    }
-    return result;
-  };
-
-  return (
-    <CustomText
-      style={[
-        props.style,
-        { fontFamily: "Nunito_400Regular" },
-        mode === "dark" && { color: "#E4E3E4" },
-      ]}
-      selectable={true}
-    >
-      {prepareText(props.children, props.mentionHashtagPress)}
-    </CustomText>
-  );
-});
+    return (
+      <CustomText
+        style={[
+          style,
+          { fontFamily: "Nunito_400Regular" },
+          mode === "dark" && { color: "#E4E3E4" },
+        ]}
+        selectable={true}
+      >
+        {prepareText(children, mentionHashtagPress)}
+      </CustomText>
+    );
+  }
+);
 
 const Mention = (props) => {
   return (
